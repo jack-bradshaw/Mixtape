084596b5d74d23d1d09908543a79784ed94e32f7
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-25 07:33:01 +1030

    Updated Javadoc

--- library/src/main/java/com/matthewtamlin/mixtape/library/base_mvp/BasePresenter.java
+++ library/src/main/java/com/matthewtamlin/mixtape/library/base_mvp/BasePresenter.java
@@ -1,17 +1,23 @@
 package com.matthewtamlin.mixtape.library.base_mvp;
 
 /**
- * The intermediary between a data source and a view, containing the business logic needed
- * to update the data and drive the view.
+ * The intermediary between a data source and a view, containing the business logic needed to update
+ * the data and drive the view.
  */
 public interface BasePresenter<S extends BaseDataSource, V extends BaseView> {
 	/**
-	 * Starts/restarts presentation by loading data from source and the updating the view.
-	 * Calling this method is safe at all times, however there may not always an effect.
+	 * Starts/restarts presentation by loading data from source and the updating the view. Calling
+	 * this method is safe at all times, however there may not always an effect.
+	 *
+	 * @param forceRefresh
+	 * 		true to discard cached data when loading from the source, false to use the default
+	 * 		behaviour
 	 */
 	void present(boolean forceRefresh);
 
 	/**
+	 * Sets the data source to present from and registers the presenter for callbacks.
+	 *
 	 * @param dataSource
 	 * 		the data source to use, null accepted
 	 */
@@ -23,6 +29,8 @@ public interface BasePresenter<S extends BaseDataSource, V extends BaseView> {
 	S getDataSource();
 
 	/**
+	 * Sets the view to present to and registers the presenter with the view.
+	 *
 	 * @param view
 	 * 		the view to use, null accepted
 	 */
