af000acfaa58d107a1e61e3461a3f512754e2062
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-25 21:44:40 +1030

    Created GridActivity
    - Created Java activity class
    - Added declaration to Android manifest

---
+++ example/src/main/java/com/matthewtamlin/mixtape/example/activities/GridActivity.java
@@ -0,0 +1,15 @@
+package com.matthewtamlin.mixtape.example.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.matthewtamlin.mixtape.example.R;
+
+public class GridActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.example_layout);
+	}
+}
