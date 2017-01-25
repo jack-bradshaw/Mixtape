18874609aa06bb758c03c678fe2944e44fdb10da
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-25 16:41:19 +1030

    Renamed Mp3Item to Mp3Song

--- example/src/main/java/com/matthewtamlin/mixtape/example/Mp3Song.java
+++ example/src/main/java/com/matthewtamlin/mixtape/example/Mp3Song.java
@@ -17,10 +17,10 @@ import java.io.File;
 
 import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
 
-public class Mp3Item implements LibraryItem {
+public class Mp3Song implements LibraryItem {
 	private File file;
 
-	public Mp3Item(final File mp3File) {
+	public Mp3Song(final File mp3File) {
 		file = checkNotNull(mp3File, "mp3File cannot be null.");
 	}
 
