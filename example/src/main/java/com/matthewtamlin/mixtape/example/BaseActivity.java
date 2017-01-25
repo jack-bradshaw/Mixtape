22c76e877b2d041bd62d509554e3209d320ac955
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-25 13:36:35 +1030

    Made class abstract and setup UI in onCreate()

--- example/src/main/java/com/matthewtamlin/mixtape/example/BaseActivity.java
+++ example/src/main/java/com/matthewtamlin/mixtape/example/BaseActivity.java
@@ -3,10 +3,28 @@ package com.matthewtamlin.mixtape.example;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BaseActivity extends AppCompatActivity {
+import com.matthewtamlin.mixtape.library.mixtape_body.RecyclerViewBody;
+import com.matthewtamlin.mixtape.library.mixtape_coordinator.CoordinatedMixtapeContainer;
+import com.matthewtamlin.mixtape.library.mixtape_header.SmallHeader;
+
+public abstract class BaseActivity extends AppCompatActivity {
+	private CoordinatedMixtapeContainer container;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_base);
+
+		container = (CoordinatedMixtapeContainer) findViewById(R.id.base_activity_root);
+		container.setHeader(getHeader());
+		container.setBody(getBody());
 	}
+
+	public CoordinatedMixtapeContainer getContainer() {
+		return container;
+	}
+
+	public abstract SmallHeader getHeader();
+
+	public abstract RecyclerViewBody getBody();
 }
