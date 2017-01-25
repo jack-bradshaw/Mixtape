b54c9a780636a3daa8a022cdaad4fc3516291439
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-25 15:34:25 +1030

    Added constructor

--- example/src/main/java/com/matthewtamlin/mixtape/example/Mp3Item.java
+++ example/src/main/java/com/matthewtamlin/mixtape/example/Mp3Item.java
@@ -2,5 +2,16 @@ package com.matthewtamlin.mixtape.example;
 
 import com.matthewtamlin.mixtape.library.data.LibraryItem;
 
+import java.io.File;
+
+import static com.matthewtamlin.java_utilities.checkers.NullChecker.checkNotNull;
+
 public class Mp3Item implements LibraryItem {
+	private File file;
+
+	public Mp3Item(final File mp3File) {
+		file = checkNotNull(mp3File, "mp3File cannot be null.");
+	}
+
+	
 }
