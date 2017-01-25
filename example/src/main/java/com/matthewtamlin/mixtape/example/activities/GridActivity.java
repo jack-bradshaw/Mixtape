96438af826b4dd738e29aa99a4c9cad379a615b9
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-25 22:16:55 +1030

    Added skeleton

--- example/src/main/java/com/matthewtamlin/mixtape/example/activities/GridActivity.java
+++ example/src/main/java/com/matthewtamlin/mixtape/example/activities/GridActivity.java
@@ -4,12 +4,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.matthewtamlin.mixtape.example.R;
+import com.matthewtamlin.mixtape.example.data.Mp3Album;
+import com.matthewtamlin.mixtape.example.data.Mp3AlbumDataSource;
+import com.matthewtamlin.mixtape.library.base_mvp.ListDataSource;
+import com.matthewtamlin.mixtape.library.mixtape_body.GridBody;
+import com.matthewtamlin.mixtape.library.mixtape_body.RecyclerViewBody;
+import com.matthewtamlin.mixtape.library.mixtape_body.RecyclerViewBodyPresenter;
+import com.matthewtamlin.mixtape.library.mixtape_coordinator.CoordinatedMixtapeContainer;
+import com.matthewtamlin.mixtape.library.mixtape_coordinator.MixtapeContainerView;
+import com.matthewtamlin.mixtape.library.mixtape_header.SmallHeader;
 
 public class GridActivity extends AppCompatActivity {
+	private CoordinatedMixtapeContainer container;
+
+	private GridBody body;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.example_layout);
+		setupView();
+
+	}
+
+	private void setupView() {
+		
+	}
+
+	private void setupBodyDataSource() {
+
+	}
+
+	private void setupPresenter() {
+
+	}
+
+	private void integrateMVP() {
+
 	}
 }
