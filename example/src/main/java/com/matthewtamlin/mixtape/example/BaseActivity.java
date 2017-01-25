2f4c5fa26989c75560bc2f3a41fb0e5418372503
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-25 11:45:54 +1030

    Created base activity
    - Created Java activity class
    - Created XML layout
    - Declared activity in manifest

---
+++ example/src/main/java/com/matthewtamlin/mixtape/example/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.matthewtamlin.mixtape.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_base);
+	}
+}
