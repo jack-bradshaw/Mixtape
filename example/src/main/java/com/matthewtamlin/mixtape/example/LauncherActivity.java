d403e95534b7d127bdfb943fc7a5dcff64bfff3c
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-25 21:39:25 +1030

    Created launcher activity
    - Created java activity
    - Created xml layout
    - Declared activity in Android manifest

---
+++ example/src/main/java/com/matthewtamlin/mixtape/example/LauncherActivity.java
@@ -0,0 +1,12 @@
+package com.matthewtamlin.mixtape.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.launcher_activity);
+	}
+}
