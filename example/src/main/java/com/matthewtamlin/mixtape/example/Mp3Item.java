b14639589257b79b5061aad03a87bdc26eb7869d
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-25 15:34:45 +1030

    Added stubs for unimplemented interface methods

--- example/src/main/java/com/matthewtamlin/mixtape/example/Mp3Item.java
+++ example/src/main/java/com/matthewtamlin/mixtape/example/Mp3Item.java
@@ -1,6 +1,10 @@
 package com.matthewtamlin.mixtape.example;
 
+import android.graphics.Bitmap;
+
 import com.matthewtamlin.mixtape.library.data.LibraryItem;
+import com.matthewtamlin.mixtape.library.data.LibraryReadException;
+import com.matthewtamlin.mixtape.library.data.LibraryWriteException;
 
 import java.io.File;
 
@@ -13,5 +17,41 @@ public class Mp3Item implements LibraryItem {
 		file = checkNotNull(mp3File, "mp3File cannot be null.");
 	}
 
-	
+	@Override
+	public CharSequence getTitle() throws LibraryReadException {
+		return null;
+	}
+
+	@Override
+	public CharSequence getSubtitle() throws LibraryReadException {
+		return null;
+	}
+
+	@Override
+	public Bitmap getArtwork(final int width, final int height) throws LibraryReadException {
+		return null;
+	}
+
+	@Override
+	public void setTitle(final CharSequence title)
+			throws LibraryReadException, LibraryWriteException {
+
+	}
+
+	@Override
+	public void setSubtitle(final CharSequence subtitle)
+			throws LibraryReadException, LibraryWriteException {
+
+	}
+
+	@Override
+	public void setArtwork(final Bitmap artwork)
+			throws LibraryReadException, LibraryWriteException {
+
+	}
+
+	@Override
+	public boolean isReadOnly() {
+		return false;
+	}
 }
