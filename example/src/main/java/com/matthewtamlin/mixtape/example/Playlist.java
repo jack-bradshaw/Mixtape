f606a2bbd999bdff0a4d12a170cc9336c3db98be
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-25 17:48:20 +1030

    Changed class to use the POJO pattern

--- example/src/main/java/com/matthewtamlin/mixtape/example/Playlist.java
+++ example/src/main/java/com/matthewtamlin/mixtape/example/Playlist.java
@@ -6,71 +6,50 @@ import com.matthewtamlin.mixtape.library.data.LibraryItem;
 import com.matthewtamlin.mixtape.library.data.LibraryReadException;
 import com.matthewtamlin.mixtape.library.data.LibraryWriteException;
 
-import org.jaudiotagger.audio.AudioFileIO;
-import org.jaudiotagger.tag.FieldKey;
-import org.jaudiotagger.tag.Tag;
-
-import java.io.File;
 import java.util.ArrayList;
 
 public class Playlist extends ArrayList<Mp3Song> implements LibraryItem {
+	private CharSequence title;
+
+	private CharSequence subtitle;
+
+	private Bitmap artwork;
+
 	@Override
 	public CharSequence getTitle() throws LibraryReadException {
-		if (isEmpty()) {
-			return "Empty album";
-		} else {
-			return readId3Field(get(0).getMp3File(), FieldKey.ALBUM);
-		}
+		return title;
 	}
 
 	@Override
 	public CharSequence getSubtitle() throws LibraryReadException {
-		if (isEmpty()) {
-			return null;
-		} else {
-			return readId3Field(get(0).getMp3File(), FieldKey.ALBUM_ARTIST);
-		}
+		return subtitle;
 	}
 
 	@Override
 	public Bitmap getArtwork(final int width, final int height) throws LibraryReadException {
-		if (isEmpty()) {
-			return null;
-		} else {
-			return get(0).getArtwork(width, height);
-		}
+		return artwork;
 	}
 
 	@Override
 	public void setTitle(final CharSequence title)
 			throws LibraryReadException, LibraryWriteException {
-
+		this.title = title;
 	}
 
 	@Override
 	public void setSubtitle(final CharSequence subtitle)
 			throws LibraryReadException, LibraryWriteException {
-
+		this.subtitle = subtitle;
 	}
 
 	@Override
 	public void setArtwork(final Bitmap artwork)
 			throws LibraryReadException, LibraryWriteException {
-
+		this.artwork = artwork;
 	}
 
 	@Override
 	public boolean isReadOnly() {
 		return false;
-	}
-
-	private String readId3Field(final File mp3File, final FieldKey fieldKey)
-			throws LibraryReadException {
-		try {
-			final Tag tag = AudioFileIO.read(mp3File).getTag();
-			return tag == null ? null : tag.getFirst(fieldKey);
-		} catch (final Exception e) {
-			throw new LibraryReadException("Cannot read ID3 tag from file " + mp3File, e);
-		}
 	}
 }
