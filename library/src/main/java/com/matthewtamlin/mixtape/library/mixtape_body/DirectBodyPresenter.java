d52645c80cb704c00b2a3abb47efcebf390ee079
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-26 21:48:47 +1030

    Added loadData call when view or data source set

--- library/src/main/java/com/matthewtamlin/mixtape/library/mixtape_body/DirectBodyPresenter.java
+++ library/src/main/java/com/matthewtamlin/mixtape/library/mixtape_body/DirectBodyPresenter.java
@@ -45,6 +45,10 @@ public abstract class DirectBodyPresenter<
 		unsubscribeFromDataSourceCallbacks(this.dataSource);
 		this.dataSource = dataSource;
 		subscribeToDataSourceCallbacks(this.dataSource);
+
+		if (dataSource != null) {
+			dataSource.loadData(false, this);
+		}
 	}
 
 	@Override
@@ -57,6 +61,10 @@ public abstract class DirectBodyPresenter<
 		removeViewPresenter(this.view);
 		this.view = view;
 		setSelfAsViewPresenter(this.view);
+
+		if (dataSource != null) {
+			dataSource.loadData(false, this);
+		}
 	}
 
 	@Override
