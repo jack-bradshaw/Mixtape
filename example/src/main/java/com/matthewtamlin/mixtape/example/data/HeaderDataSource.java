0e0a601f1ecb9c76d68a18f264dd79b58861d7f5
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-28 00:00:42 +1030

    Deleted method setData

--- example/src/main/java/com/matthewtamlin/mixtape/example/data/HeaderDataSource.java
+++ example/src/main/java/com/matthewtamlin/mixtape/example/data/HeaderDataSource.java
@@ -6,18 +6,6 @@ import com.matthewtamlin.mixtape.library.data.LibraryItem;
 public class HeaderDataSource extends BaseDataSourceAdapter<LibraryItem> {
 	private LibraryItem item;
 
-	public void setData(final LibraryItem item) {
-		final LibraryItem oldData = this.item;
-
-		if (this.item != item) {
-			this.item = item;
-
-			for (final DataReplacedListener<LibraryItem> listener : getDataReplacedListeners()) {
-				listener.onDataReplaced(this, oldData, item);
-			}
-		}
-	}
-
 	@Override
 	public void loadData(final boolean forceRefresh,
 			final DataLoadedListener<LibraryItem> callback) {
