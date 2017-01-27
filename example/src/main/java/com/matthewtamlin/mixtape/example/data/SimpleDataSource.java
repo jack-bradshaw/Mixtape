ed9529cb0d7d7584ae2e607c6b910c07a94c13c8
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-01-27 22:21:31 +1030

    Created SimpleDataSource

---
+++ example/src/main/java/com/matthewtamlin/mixtape/example/data/SimpleDataSource.java
@@ -0,0 +1,30 @@
+package com.matthewtamlin.mixtape.example.data;
+
+import com.matthewtamlin.mixtape.library.data.BaseDataSourceAdapter;
+import com.matthewtamlin.mixtape.library.data.LibraryItem;
+
+public class SimpleDataSource extends BaseDataSourceAdapter<LibraryItem> {
+	private LibraryItem item;
+
+	public void setData(final LibraryItem item) {
+		final LibraryItem oldData = this.item;
+
+		if (this.item != item) {
+			this.item = item;
+
+			for (final DataReplacedListener<LibraryItem> listener : getDataReplacedListeners()) {
+				listener.onDataReplaced(this, oldData, item);
+			}
+		}
+	}
+
+	@Override
+	public void loadData(final boolean forceRefresh,
+			final DataLoadedListener<LibraryItem> callback) {
+		if (item != null) {
+			callback.onDataLoaded(this, item);
+		} else {
+			callback.onLoadDataFailed(this);
+		}
+	}
+}
