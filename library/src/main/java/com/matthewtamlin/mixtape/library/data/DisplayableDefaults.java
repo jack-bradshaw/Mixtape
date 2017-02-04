e1bd31856415f14244316cac081cc35d8ac8ed4a
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-02-04 16:03:35 +1030

    Updated Javadoc

--- library/src/main/java/com/matthewtamlin/mixtape/library/data/DisplayableDefaults.java
+++ library/src/main/java/com/matthewtamlin/mixtape/library/data/DisplayableDefaults.java
@@ -23,24 +23,24 @@ import android.graphics.Bitmap;
  */
 public interface DisplayableDefaults {
 	/**
-	 * Supplies the default title. The same title does not need to be returned each time this method
-	 * is called, and null may be returned.
+	 * Supplies the default title. The returned value does not need to be consistent between
+	 * calls, and null may be returned.
 	 *
 	 * @return the default title
 	 */
 	CharSequence getTitle();
 
 	/**
-	 * Supplies the default subtitle. The same subtitle does not need to be returned each time this
-	 * method is called, and null may be returned.
+	 * Supplies the default subtitle. The returned value does not need to be consistent between
+	 * calls, and null may be returned.
 	 *
 	 * @return the default subtitle
 	 */
 	CharSequence getSubtitle();
 
 	/**
-	 * Supplies the default artwork. The same artwork does not need to be returned each time this
-	 * method is called, and null may be returned.
+	 * Supplies the default artwork. The returned value does not need to be consistent between
+	 * calls, and null may be returned.
 	 *
 	 * @return the default artwork
 	 */
