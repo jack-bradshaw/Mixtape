828e214675af0370bd7baa3213799f530d8702ca
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-02-09 18:28:54 +1030

    Added support for LruCache architecture

--- example/src/main/java/com/matthewtamlin/mixtape/example/activities/LauncherActivity.java
+++ example/src/main/java/com/matthewtamlin/mixtape/example/activities/LauncherActivity.java
@@ -18,8 +18,8 @@ package com.matthewtamlin.mixtape.example.activities;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
