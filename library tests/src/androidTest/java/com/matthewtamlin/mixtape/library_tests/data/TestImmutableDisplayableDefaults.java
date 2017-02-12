525e7ab0fc0dd997ca71dc16731ae2c65802e998
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-02-12 17:50:55 +1030

    Deleted existing tests

--- library tests/src/androidTest/java/com/matthewtamlin/mixtape/library_tests/data/TestImmutableDisplayableDefaults.java
+++ library tests/src/androidTest/java/com/matthewtamlin/mixtape/library_tests/data/TestImmutableDisplayableDefaults.java
@@ -16,61 +16,11 @@
 
 package com.matthewtamlin.mixtape.library_tests.data;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-
-import com.matthewtamlin.mixtape.library.data.DisplayableDefaults;
-import com.matthewtamlin.mixtape.library.data.ImmutableDisplayableDefaults;
-import com.matthewtamlin.mixtape.library_tests.test.R;
-
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static android.graphics.BitmapFactory.decodeResource;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 
 @RunWith(AndroidJUnit4.class)
 public class TestImmutableDisplayableDefaults {
-	private static final String TITLE = "cake";
-	private static final String SUBTITLE = "lie";
-	private static final int IMAGE_RES_ID = R.raw.image1;
-
-	private Drawable artwork;
-
-	private DisplayableDefaults displayableDefaults;
-
-	@Before
-	public void setup() {
-		// Decode an image for use in testing
-		final Context context = InstrumentationRegistry.getTargetContext();
-		artwork = new BitmapDrawable(context.getResources(), BitmapFactory.decodeResource(context
-				.getResources(), IMAGE_RES_ID));
-		assertThat("precondition failed: artwork is null", artwork, is(notNullValue()));
-
-		displayableDefaults = new ImmutableDisplayableDefaults(TITLE, SUBTITLE, artwork);
-	}
-
-	@Test
-	public void testGetTitle_shouldReturnTestTitle() {
-		assertThat(displayableDefaults.getTitle(), is((CharSequence) TITLE));
-	}
-
-	@Test
-	public void testGetSubtitle_shouldReturnTestSubtitle() {
-		assertThat(displayableDefaults.getSubtitle(), is((CharSequence) SUBTITLE));
-	}
-
-	@Test
-	public void testGetArtwork_shouldReturnTestArtwork() {
-		assertThat(displayableDefaults.getArtwork(), is(artwork));
-	}
+	
 }
