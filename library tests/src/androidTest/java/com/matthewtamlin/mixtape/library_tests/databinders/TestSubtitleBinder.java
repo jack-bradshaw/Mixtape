111498845f83fb8d533dac694bf3bf57ae6f8e41
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-02-13 17:40:15 +1030

    Added getter tests to existing test case

--- library tests/src/androidTest/java/com/matthewtamlin/mixtape/library_tests/databinders/TestSubtitleBinder.java
+++ library tests/src/androidTest/java/com/matthewtamlin/mixtape/library_tests/databinders/TestSubtitleBinder.java
@@ -30,6 +30,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
@@ -118,7 +120,10 @@ public class TestSubtitleBinder {
 	 */
 	@Test
 	public void testConstructor_validArgs() {
-		new SubtitleBinder(cache, displayableDefaults);
+		final SubtitleBinder binder = new SubtitleBinder(cache, displayableDefaults);
+
+		assertThat("Incorrect cache.", binder.getCache(), is(cache));
+		assertThat("Incorrect defaults.", binder.getDefaults(), is(displayableDefaults));
 	}
 
 	/**
