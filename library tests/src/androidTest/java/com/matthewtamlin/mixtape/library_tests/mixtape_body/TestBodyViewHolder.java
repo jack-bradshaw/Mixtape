a81a32ebe42df59b45104f95d8a7892bbaad91cd
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-02-15 13:37:25 +1030

    Deleted existing tests

--- library tests/src/androidTest/java/com/matthewtamlin/mixtape/library_tests/mixtape_body/TestBodyViewHolder.java
+++ library tests/src/androidTest/java/com/matthewtamlin/mixtape/library_tests/mixtape_body/TestBodyViewHolder.java
@@ -16,18 +16,10 @@
 
 package com.matthewtamlin.mixtape.library_tests.mixtape_body;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.matthewtamlin.mixtape.library.mixtape_body.BodyViewHolder;
-import com.matthewtamlin.mixtape.library_tests.test.R;
 
-import org.junit.Before;
 import org.junit.runner.RunWith;
 
 /**
@@ -36,39 +28,5 @@ import org.junit.runner.RunWith;
 @SuppressWarnings("ResourceType") // Using mock Views so don't need to use actual resource IDs
 @RunWith(AndroidJUnit4.class)
 public class TestBodyViewHolder {
-	/**
-	 * A view for use in testing.
-	 */
-	private View rootView;
-
-	/**
-	 * A TextView for use in testing. This view is the child of the root view.
-	 */
-	private TextView titleView;
-
-	/**
-	 * A TextView for use in testing. This view is the child of the root view.
-	 */
-	private TextView subtitleView;
-
-	/**
-	 * An ImageView for use in testing. This view is the child of the root view.
-	 */
-	private ImageView artworkView;
-
-	/**
-	 * A View for use in testing. This view is the child of the root view.
-	 */
-	private View contextualMenuButton;
-
-	@Before
-	public void setup() {
-		// Inflate the root view and get references to its children
-		final Context context = InstrumentationRegistry.getContext();
-		rootView = LayoutInflater.from(context).inflate(R.layout.testbodyviewholder, null);
-		titleView = (TextView) rootView.findViewById(R.id.testBodyViewHolder_titleView);
-		subtitleView = (TextView) rootView.findViewById(R.id.testBodyViewHolder_subtitleView);
-		artworkView = (ImageView) rootView.findViewById(R.id.testBodyViewHolder_artworkView);
-		contextualMenuButton = rootView.findViewById(R.id.testBodyViewHolder_contextualMenuButton);
-	}
+	
 }
