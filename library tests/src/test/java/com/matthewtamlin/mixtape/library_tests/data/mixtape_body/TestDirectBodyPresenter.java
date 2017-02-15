04297a50f52260730023bfc56e2e4513a1425330
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-02-15 21:32:34 +1030

    Wrote test testOnLibraryItemSelected_noCallbacks

--- library tests/src/test/java/com/matthewtamlin/mixtape/library_tests/data/mixtape_body/TestDirectBodyPresenter.java
+++ library tests/src/test/java/com/matthewtamlin/mixtape/library_tests/data/mixtape_body/TestDirectBodyPresenter.java
@@ -548,7 +548,7 @@ public class TestDirectBodyPresenter {
 
 	@Test
 	public void testOnLibraryItemSelected_noCallbacks() {
-
+		presenter.onLibraryItemSelected(mock(View.class), mock(LibraryItem.class));
 	}
 
 	@Test
