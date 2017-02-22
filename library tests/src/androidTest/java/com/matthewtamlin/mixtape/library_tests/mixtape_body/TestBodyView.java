9eadec154153fbc010291f3ac0b174d14b6f6d35
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-02-23 09:01:41 +1030

    Implemented tests

--- library tests/src/androidTest/java/com/matthewtamlin/mixtape/library_tests/mixtape_body/TestBodyView.java
+++ library tests/src/androidTest/java/com/matthewtamlin/mixtape/library_tests/mixtape_body/TestBodyView.java
@@ -16,26 +16,55 @@
 
 package com.matthewtamlin.mixtape.library_tests.mixtape_body;
 
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.runner.AndroidJUnit4;
+
+import com.matthewtamlin.mixtape.library.data.LibraryItem;
+import com.matthewtamlin.mixtape.library.mixtape_body.BodyView;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+
+@SuppressWarnings("unchecked")
 @RunWith(AndroidJUnit4.class)
 public abstract class TestBodyView {
 	@Test
 	public void testSetAndGetItems() {
+		final List<LibraryItem> items = new ArrayList<>();
+		final LibraryItem item1 = mock(LibraryItem.class);
+		final LibraryItem item2 = mock(LibraryItem.class);
+		items.add(item1);
+		items.add(item2);
 
+		getBodyViewDirect().setItems(items);
+
+		assertThat("Returned list has wrong size.", getBodyViewDirect().getItems().size(),
+				is(2));
+		assertThat("Returned list doesn't contain item 1.", getBodyViewDirect().getItems()
+				.contains(item1), is(true));
+		assertThat("Returned list doesn't contain item 1.", getBodyViewDirect().getItems()
+				.contains(item2), is(true));
 	}
 
 	@Test
 	public void testSetAndGetContextualMenuResource() {
+		final int id = 10010010;
 
+		getBodyViewDirect().setContextualMenuResource(id);
+
+		assertThat("Returned wrong ID.", getBodyViewDirect().getContextualMenuResource(), is(id));
 	}
 
 	@Test
 	public void testAddLibraryItemSelectedListener() {
-
+		
 	}
 
 	@Test
@@ -52,4 +81,8 @@ public abstract class TestBodyView {
 	public void testRemoveMenuItemSelectedListener() {
 
 	}
+
+	public abstract BodyView getBodyViewDirect();
+
+	public abstract ViewInteraction getBodyViewEspresso();
 }
