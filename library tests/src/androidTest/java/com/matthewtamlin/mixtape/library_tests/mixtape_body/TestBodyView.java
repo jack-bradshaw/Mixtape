5e73f9aeba2ce5b6bf28b96b466a1239dd8e83db
Matthew Tamlin <matthew.tamlin@icloud.com>
2017-02-22 15:18:43 +1030

    Made class abstract

--- library tests/src/androidTest/java/com/matthewtamlin/mixtape/library_tests/mixtape_body/TestBodyView.java
+++ library tests/src/androidTest/java/com/matthewtamlin/mixtape/library_tests/mixtape_body/TestBodyView.java
@@ -22,7 +22,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-public class TestBodyView {
+public abstract class TestBodyView {
 	@Test
 	public void testSetAndGetItems() {
 
